59d04f8e67bbb567f788b34e4d549161efc3c5a2
algor1 <alexeev.igor.v@gmail.com>
2016-08-02 11:37:17 +0300

    Update tensor-minst.py

--- tensor-minst.py
+++ tensor-minst.py
@@ -60,23 +60,28 @@ for i in range(len(ans[0])):
 
 
 def drawPicture(pix3):
-  colR=0
-  colB=0
+  
   image3 = Image.new("RGB",(28,28),color=(255,255,255))  
-  draw3 = ImageDraw.Draw(image3)   
+  draw3 = ImageDraw.Draw(image3)  
+  kBlue=-255/min(pix3)
+  kRed=255/max(pix3)
+  print (max(pix3), min(pix3))
+  
   for i in range(0,28):
     for j in range(0,28):
-      print (max(pix3), min(pix3))
-      if int(pix3[i*28+j,4]*255)>0:
-        colR=int(pix3[i*28+j,4]*255)
+      if pix3[i*28+j,4]>0:
+        colR=int(pix3[i*28+j,4]*kRed)
         colB=0
       else:
-        colB=-int(pix3[i*28+j,4]*255)
+        colB=int(pix3[i*28+j,4]*kBlue)
         colR=0
-      draw3.point((i, j),fill=(colR,colR,colR))
+      draw3.point((i, j),fill=(colR,0,colB))
   image3.show()
 
 pix33=sess.run(W)
 print(len(pix33))
-drawPicture(pix33)
+for k in range(10):
+  for i in range (784):
+    outputpixarray[i]=pix33[i,k]
+  drawPicture(outputPixArray)
 
