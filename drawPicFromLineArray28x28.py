3db5a51b9a3f9b2d0203a17329a00073a92e3408
algor1 <alexeev.igor.v@gmail.com>
2016-08-02 15:24:45 +0300

    Update drawPicFromLineArray28x28.py

--- drawPicFromLineArray28x28.py
+++ drawPicFromLineArray28x28.py
@@ -1,37 +1,49 @@
+from PIL import Image, ImageDraw
+
 def drawPicture(pix3):
   image3 = Image.new("RGB",(28,28),color=(255,255,255))  
   draw3 = ImageDraw.Draw(image3)  
-  kBlue=-255/min(pix3)
-  kRed=255/max(pix3)
+  if min(pix3)>=0:
+    kBlue=0
+  else:
+    kBlue=-255/min(pix3)
+  if max(pix3)<=0:
+    kRed=0
+  else:
+    kRed=255/max(pix3)
+    
   print (max(pix3), min(pix3))
   
   for i in range(0,28):
     for j in range(0,28):
-      if pix3[i*28+j,4]>0:
-        colR=int(pix3[i*28+j,4]*kRed)
+      x=i*28+j
+      if pix3[x]>0:
+        colR=int((pix3[x])*kRed)
         colB=0
       else:
-        colB=int(pix3[i*28+j,4]*kBlue)
+        colB=int((pix3[x])*kBlue)
         colR=0
       draw3.point((i, j),fill=(colR,0,colB))
   image3.show()
 
 
 #добываем картинку
-pix33=sess.run(W)
+#pix33=sess.run(W)
 #или
 inputSubArray=[]
+pix33=[]
 nnn=0
+outputPixArray=[]
 for i in range (784):
   if i == nnn:
     nnn+=29
-    inputSubArray.append(1)
+    inputSubArray.append([1,1,1,1,1,1,1,1,1,1])
   else:
-    inputSubArray.append(0)
-print(inputSubArray)
+    inputSubArray.append([0,0,0,0,0,0,0,0,0,0])
+pix33=inputSubArray
+print(len(pix33[2]))
 
-print(len(pix33))
 for k in range(10):
   for i in range (784):
-    outputpixarray[i]=pix33[i,k]
+    outputPixArray.append(pix33[i][k])
   drawPicture(outputPixArray)
