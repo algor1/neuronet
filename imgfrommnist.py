d25f2f5903ec0bd7607a26ad62de4ef1cf121420
algor1 <algor1@users.noreply.github.com>
2016-08-08 17:59:34 +0300

    Update imgfrommnist.py

--- imgfrommnist.py
+++ imgfrommnist.py
@@ -9,25 +9,25 @@ from PIL import Image, ImageDraw
 p=mnist.test.images
 l=mnist.test.labels
 for i in range(100):
-    if l[i]==[1,0,0,0,0,0,0,0,0,0]:
+    if l[i][0]==1:
         m0.append(p[i])
-    elif l[i]==[0,1,0,0,0,0,0,0,0,0]:
+    elif l[i][1]==1:
         m1.append(p[i])
-    elif l[i]==[0,0,1,0,0,0,0,0,0,0]:
+    elif l[i][2]==1:
         m2.append(p[i])
-    elif l[i]==[0,0,0,1,0,0,0,0,0,0]:
+    elif l[i][3]==1:
         m3.append(p[i])
-    elif l[i]==[0,0,0,0,1,0,0,0,0,0]:
+    elif l[i][4]==1:
         m4.append(p[i])
-    elif l[i]==[0,0,0,0,0,1,0,0,0,0]:
+    elif l[i][5]==1:
         m5.append(p[i])
-    elif l[i]==[0,0,0,0,0,0,1,0,0,0]:
+    elif l[i][6]==1:
         m6.append(p[i])
-    elif l[i]==[0,0,0,0,0,0,0,1,0,0]:
+    elif l[i][7]==1:
         m7.append(p[i])
-    elif l[i]==[0,0,0,0,0,0,0,0,1,0]:
+    elif l[i][8]==1:
         m8.append(p[i])
-    elif l[i]==[0,0,0,0,0,0,0,0,0,1]:
+    elif l[i][9]==1:
         m9.append(p[i])
 
 drbanch(m0).show
