4fbc834a2ad75f9228e0ff4cbf494f8c5ca49ba2
algor1 <algor1@users.noreply.github.com>
2016-08-09 18:41:54 +0300

    Update client.py

--- client.py
+++ client.py
@@ -9,15 +9,12 @@ tcp_socket = socket(AF_INET, SOCK_STREAM)
 tcp_socket.connect(addr)
 
 
-data = input('write to server: ')
-if not data : 
+filename = input('имя файла: ')
+if not filename: 
     tcp_socket.close() 
     sys.exit(1)
+sock.send( open(filename, "rb").read() )
 
-#encode - перекодирует введенные данные в байты, decode - обратно
-data = str.encode(data)
-tcp_socket.send(data)
-data = bytes.decode(data)
 data = tcp_socket.recv(1024)
 print(data)
 
