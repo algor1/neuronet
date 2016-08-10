10cad97c25e69d5aa85a81a5d62230d48f0966e2
algor1 <algor1@users.noreply.github.com>
2016-08-10 22:43:58 +0400

    Update servertw.py

--- servertw.py
+++ servertw.py
@@ -15,7 +15,7 @@ class Twist(protocol.Protocol):
     
     #Событие connectionMade срабатывает при соединении
     def connectionMade(self):
-        print 'connection success!'
+        print ('connection success!')
     
     #Событие dataReceived - получение и отправление данных
     def dataReceived(self, data):
@@ -25,11 +25,11 @@ class Twist(protocol.Protocol):
     
     #Событие connectionLost срабатывает при разрыве соединения с клиентом
     def connectionLost(self, reason):
-        print 'Connection lost!'
+        print ('Connection lost!')
 
 #Конфигурация поведения протокола описывается в – классе Factory из twisted.internet.protocol.Factory
 factory = protocol.Factory()
 factory.protocol = Twist
-print 'wait...'
+print ('wait...')
 reactor.listenTCP(777, factory)
 reactor.run()
