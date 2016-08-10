52ad13e66971ded97083544525a77370850d40ac
algor1 <algor1@users.noreply.github.com>
2016-08-10 22:45:22 +0400

    Update clienttw.py

--- clienttw.py
+++ clienttw.py
@@ -17,18 +17,18 @@ class Twist_client(protocol.Protocol):
         self.sendData()
         
     def dataReceived(self, data):
-        print data
+        print (data)
         self.sendData()
         
 class Twist_Factory(protocol.ClientFactory):
     protocol = Twist_client
     
     def clientConnectionFailed(self, connector, reason):
-        print 'connection failed:', reason.getErrorMessage()
+        print ('connection failed:', reason.getErrorMessage())
         reactor.stop()
 
     def clientConnectionLost(self, connector, reason):
-        print 'connection lost:', reason.getErrorMessage()
+        print ('connection lost:', reason.getErrorMessage())
         reactor.stop()
         
 factory = Twist_Factory()
